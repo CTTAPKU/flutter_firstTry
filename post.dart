0528c641b353dc92b2bac08210d9bf788e50682d
Klunnyk Dmytro <117164181+CTTAPKU@users.noreply.github.com>
2024-09-15 16:21:54 +0300

    Update post.dart

--- post.dart
+++ post.dart
@@ -1,17 +1,16 @@
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-
 class Post {
    String? urlToImage;
    String? title;
    String? name;
    String? description;
+   String? url;
 
   Post({
      this.urlToImage,
      this.title,
      this.name,
-     this.description
+     this.description,
+     this.url
   });
 
 }
